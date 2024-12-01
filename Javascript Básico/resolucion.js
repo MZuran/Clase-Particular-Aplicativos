9d2b0edf233cfe55295b0cf84eb518a57a886bf3
MZuran <mzuran@frba.utn.edu.ar>
2024-12-01 09:59:08 -0300

    Añadida clase 2

--- Javascript Básico/resolucion.js
+++ Javascript Básico/resolucion.js
@@ -6,8 +6,39 @@ console.log(personas)
 
 /* --------------------------- EJERCICIO 2 --------------------------- */
 
+console.log(productos)
+
+const productosMapeados = productos.map(
+    elemento => (
+        { nombre: elemento.nombre, precio_ajustado: elemento.precio - (elemento.precio * elemento.descuento()) }
+    )
+)
+console.log(productosMapeados)
+
+productos.forEach(
+    elemento => {
+        console.log(elemento.nombre + " tiene un descuento de " + elemento.precio * elemento.descuento() );
+    }
+)
 
 /* --------------------------- EJERCICIO 3 --------------------------- */
 
+function sumaDeNotas(estudiante) {
+    
+    let sumaTotal = 0;
+
+    estudiante.notas.forEach(
+        elemento => sumaTotal = sumaTotal + elemento
+    );
+
+    return sumaTotal;
+}
+
+// Acordarse que en el mapeo, el parámetro puede tener cualquier nombre
+// Generalmente le ponemos "elemento", pero aquí se llama "estudiante"
+
+const estudiantesMapeados = estudiantes.map(
+    estudiante => ({ nombre: estudiante.nombre, promedio: sumaDeNotas(estudiante) / estudiante.notas.length })
+);
 
 /* --------------------------- EJERCICIO 4 --------------------------- */
