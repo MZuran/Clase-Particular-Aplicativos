c6bde08f28e62d0a9ee5e22cbf88cf9561a2a82c
MZuran <mzuran@frba.utn.edu.ar>
2024-12-13 09:39:02 -0300

    Borrada la resolucion del ejercicio de javascript

--- 1 - Javascript Básico/resolucion.js
+++ 1 - Javascript Básico/resolucion.js
@@ -1,44 +1,15 @@
 // Hace las resoluciones acá
 
+console.log("Esta es la consola del programador.")
+console.log("Mostra los resultados por acá")
+
 /* --------------------------- EJERCICIO 1 --------------------------- */
 
-console.log(personas)
-
+console.log("El array de personas del ejercicio 1 es", personas)
 /* --------------------------- EJERCICIO 2 --------------------------- */
 
-console.log(productos)
-
-const productosMapeados = productos.map(
-    elemento => (
-        { nombre: elemento.nombre, precio_ajustado: elemento.precio - (elemento.precio * elemento.descuento()) }
-    )
-)
-console.log(productosMapeados)
-
-productos.forEach(
-    elemento => {
-        console.log(elemento.nombre + " tiene un descuento de " + elemento.precio * elemento.descuento() );
-    }
-)
 
 /* --------------------------- EJERCICIO 3 --------------------------- */
 
-function sumaDeNotas(estudiante) {
-    
-    let sumaTotal = 0;
-
-    estudiante.notas.forEach(
-        elemento => sumaTotal = sumaTotal + elemento
-    );
-
-    return sumaTotal;
-}
-
-// Acordarse que en el mapeo, el parámetro puede tener cualquier nombre
-// Generalmente le ponemos "elemento", pero aquí se llama "estudiante"
-
-const estudiantesMapeados = estudiantes.map(
-    estudiante => ({ nombre: estudiante.nombre, promedio: sumaDeNotas(estudiante) / estudiante.notas.length })
-);
 
 /* --------------------------- EJERCICIO 4 --------------------------- */
