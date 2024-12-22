a1c48bdd9e7a34637ebb4765c3102e189c1aad11
MZuran <mzuran@frba.utn.edu.ar>
2024-12-22 20:11:17 -0300

    Añadida la clase del 22 de diciembre

--- 1 - Javascript Básico/resolucion.js
+++ 1 - Javascript Básico/resolucion.js
@@ -5,16 +5,8 @@ console.log("Mostra los resultados por acá")
 
 /* --------------------------- EJERCICIO 1 --------------------------- */
 
-console.log("El array de personas del ejercicio 1 es", personas)
 /* --------------------------- EJERCICIO 2 --------------------------- */
 
-productos.forEach(
-    (elemento) => {
-        console.log(elemento.nombre + " tiene un descuento de " + elemento.precio * elemento.descuento())
-    }
-)
-
 /* --------------------------- EJERCICIO 3 --------------------------- */
-
 
 /* --------------------------- EJERCICIO 4 --------------------------- */
