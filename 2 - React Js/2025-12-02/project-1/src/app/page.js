24ea988d2294aa88ae04a18d981e4a99a6c05eff
MZuran <mzuran@frba.utn.edu.ar>
2025-12-02 16:04:48 -0300

    Añadido el contenido del 02/12

---
+++ 2 - React Js/2025-12-02/project-1/src/app/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PrimerComponente } from '@/components/primerComponente'
+import Contador from '@/components/Contador'
+
+function Page() {
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <Contador/>
+    </div>
+  )
+}
+
+export default Page
