50d909bd41ab0da883782f4aa206638d02941e59
MZuran <mzuran@frba.utn.edu.ar>
2025-12-10 17:28:26 -0300

    Añadida la clase del 10 de diciembre

--- 2 - React Js/2025-12-08/primer-proyecto/src/app/page.js
+++ 2 - React Js/2025-12-08/primer-proyecto/src/app/page.js
@@ -1,5 +1,9 @@
+"use client"
+
 import React from 'react'
 import { PrimerComponente } from '@/components/primerComponente'
+
+import { useRouter } from 'next/navigation'
 
 /*
   ¿Cómo usamos nuestro componente?
@@ -8,9 +12,33 @@ import { PrimerComponente } from '@/components/primerComponente'
 */
 
 function Page() {
+
+  const router = useRouter()
+
+  function clickHandlerUseState() { router.push("/ejemploUseState") }
+  function clickHandlerUseEffect() { router.push("/ejemploUseEffect") }
+  function clickHandlerConditionalRendering() { router.push("/ejemploConditionalRendering") }
+  function clickHandlerUseSearchParams() { 
+
+    const nombre = prompt("Ingrese su nombre")
+    const edad = prompt("Ingrese su edad")
+
+    const ruta = "/ejemploUseSearchParams?nombre=" + nombre + "&edad=" + edad
+
+    router.push(ruta) 
+  }
+
   return (
     <div>
       <h1>Home</h1>
+      <hr/>
+      <h3>Navegación </h3>
+
+      <button onClick={clickHandlerUseState} >Ir al ejemplo de useState</button>
+      <button onClick={clickHandlerUseEffect} >Ir al ejemplo de useEffect</button>
+      <button onClick={clickHandlerConditionalRendering} >Ir al ejemplo de conditional rendering</button>
+      <button onClick={clickHandlerUseSearchParams} >Ir al ejemplo de useSearchParams</button>
+
       <PrimerComponente nombre={"Matias"} ></PrimerComponente>
       <PrimerComponente nombre={"Alex"} ></PrimerComponente>
       <PrimerComponente nombre={"Ramiro"} ></PrimerComponente>
